eca2bd9616bfff786fd9dfcad092e0fcf04515a1
Rahul_Singh <koderrahul@gmail.com>
2023-01-10 21:28:18 +0530

    done in last 7 days

---
+++ wagmitutorial/pages/_app.js
@@ -0,0 +1,37 @@
+import dynamic from "next/dynamic";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, polygonMumbai],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+ function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+       <Component {...pageProps} />
+     </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default dynamic (() => Promise.resolve(App), {ssr: false})
---
+++ wagmitutorial/pages/index.js
@@ -0,0 +1,56 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import styles from '../styles/Home.module.css'
+import contractABI from "../data.json"
+import { useContractRead,
+         useContractWrite,
+         usePrepareContractWrite,
+         useWaitForTransaction  } from 'wagmi'
+
+import { useEffect, useState } from 'react'
+
+
+export default function Home() {
+  const[number,setNumber] = useState(0)
+
+  const { data :useContractReadData } = useContractRead({
+    address: '0x88cA02A16E98182FCeE19d550Dbd28355ae61779',
+    abi: contractABI.abi,
+    functionName: 'getNumber',
+  })
+
+  const { config } = usePrepareContractWrite({
+    address: '0x88cA02A16E98182FCeE19d550Dbd28355ae61779',
+    abi: contractABI.abi,
+    functionName: 'changeNumber',
+    args: [number],
+
+  })
+
+  const {data : useContractWriteData , write } = useContractWrite(config)
+
+  const {data : useWaitForTransactionData, isSuccess} = useWaitForTransaction({
+    hash: useContractWriteData?.hash
+  })
+
+  useEffect(() => {
+    console.log("__________________________");
+    console.log("useContractReadData", useContractReadData);
+    console.log("useContractWriteData:", useContractWriteData);
+    console.log("useWaitForTransactionData:", useWaitForTransactionData);
+    console.log("__________________________");
+  }, [useContractReadData, useContractWriteData, useWaitForTransactionData]);
+
+
+  return (
+    <>
+      <main className={styles.main}>
+        <ConnectButton />
+        <p>Enter New Number: </p>
+        <input onChange={(e)=>setNumber(e.target.value)} type="number" />
+        <button disabled={!write} onClick={() =>{write?.()}}>Change Number </button> 
+        {isSuccess &&<div >{useContractReadData.toNumber()}</div>} 
+
+      </main>
+    </>
+  )
+}
