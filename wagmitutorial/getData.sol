d128e98ca72a6851550892bfab710f76daef08e1
rahulsingh3526 <92939339+rahulsingh3526@users.noreply.github.com>
2023-01-11 20:18:05 +0530

    Create getData.sol

---
+++ wagmitutorial/getData.sol
@@ -0,0 +1,20 @@
+ //SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+contract GetData{
+
+       uint number;
+
+
+    function getNumber() public view returns(uint256){
+        return number;
+    }
+
+    function changeNumber(uint256 _newNumber) public returns(uint256){
+
+         number = _newNumber;
+         return number;
+    }
+
+}
